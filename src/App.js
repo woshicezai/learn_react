49317e86e9903cda34ca28e4477b28ccdfcff0eb
zhouce <zhouce@hualala.com>
2020-12-04 09:07:59 +0800

    feat: learn react

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  componentDidMount() {
+  }
+    
+  render() {
+    return (
+      <div className="App">
+          <h1>我是标题</h1>
+          <p>我是第一段话</p>
+          <p>我是第二段话</p>
+      </div>
+    );
+  }
+}
+
+export default App;
